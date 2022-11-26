a7e585cc77f0208e7c803948fa4cc85bb9d5bb5b
Marcello Prado <marcellopradomuller@gmail.com>
2022-11-26 09:00:04 -0300

    Mais uma vez...

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,4 +1,4 @@
-import { baseUrl } from './src/scripts/services/variables.js'
+import { baseUrl } from 'src/scripts/services/user.js'
 
 async function getUser(userName) {
     const response = await fetch(`${baseUrl}/${userName}`)
